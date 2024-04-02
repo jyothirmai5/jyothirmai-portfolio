ce8bd0229e96d073dd4d8be700efc32b9ab93fe7
jyothirmai5 <chjyothirmai5@gmail.com>
2024-04-01 23:48:45 -0400

    Added tooltips

--- src/components/molecules/TechStack/TechStack.tsx
+++ src/components/molecules/TechStack/TechStack.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "react";
 import styles from './TechStack.module.scss';
+import { Fade, Tooltip } from "@mui/material";
 
 interface TechStackProps {
 
@@ -10,18 +11,30 @@ const TechStack: FunctionComponent<TechStackProps> = () => {
         <div className={styles['tech-stack-div']}>
             <p className="font-primary-color">Tech Stack</p>
             <ul className={styles.skills}>
-                <li>
-                    <img src="https://skillicons.dev/icons?i=html,css,scss"></img>
-                </li>
-                <li>
-                    <img src="https://skillicons.dev/icons?i=js,ts"></img>
-                </li>
-                <li>
-                    <img src="https://skillicons.dev/icons?i=react,angular,nextjs"></img>
-                </li>
-                <li>
-                    <img src="https://skillicons.dev/icons?i=figma,xd,photoshop,aftereffects"></img>
-                </li>
+                <Tooltip sx={{ fontSize: '1rem' }} title="HTML, CSS, SCSS" TransitionComponent={Fade} placement="top"
+                    TransitionProps={{ timeout: 600 }} arrow>
+                    <li>
+                        <img src="https://skillicons.dev/icons?i=html,css,scss"></img>
+                    </li>
+                </Tooltip>
+                <Tooltip title="JavaScript, TypeScript" TransitionComponent={Fade} placement="top"
+                    TransitionProps={{ timeout: 600 }} arrow>
+                    <li>
+                        <img src="https://skillicons.dev/icons?i=js,ts"></img>
+                    </li>
+                </Tooltip>
+                <Tooltip title="React, Angular" TransitionComponent={Fade} placement="top"
+                    TransitionProps={{ timeout: 600 }} arrow>
+                    <li>
+                        <img src="https://skillicons.dev/icons?i=react,angular"></img>
+                    </li>
+                </Tooltip>
+                <Tooltip title="Figma, XD, Illustrator, Photoshop" TransitionComponent={Fade} placement="top"
+                    TransitionProps={{ timeout: 600 }} arrow>
+                    <li>
+                        <img src="https://skillicons.dev/icons?i=figma,xd,illustrator,photoshop"></img>
+                    </li>
+                </Tooltip>
             </ul>
         </div>
     );
